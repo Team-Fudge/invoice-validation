99cd27c463345318f1beccbcdedda313b0a5208b
Jasmine <jasmine.m.wong@student.unsw.edu.au>
2022-03-11 20:46:16 +1100

    testing schema validation function

---
+++ schema_validation.py
@@ -0,0 +1,20 @@
+'''
+Jasmine Wong 2022
+'''
+
+import xmlschema
+from error import AccessError, InputError
+
+def schema_validation(invoice):
+    schema = xmlschema.XMLSchema('schema_rules.xsd')
+    validation_error_iterator = schema.iter_errors(invoice)
+    errors = list()
+    for idx, validation_error in enumerate(validation_error_iterator, start=1):
+        err = validation_error.__str__()
+        errors.append(err)
+        print(err)
+    return errors
+
+
+if __name__ == "__main__":
+    schema_validation('invoice_sample.xml')
