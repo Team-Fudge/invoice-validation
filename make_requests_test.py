c8b46a35e15a1b3eb8a55d61810418444c51ddf2
Trissha <trisshavarman@gmail.com>
2022-03-12 18:03:47 +1100

    removed src folder for now

---
+++ make_requests_test.py
@@ -0,0 +1,11 @@
+import json
+from flask.globals import request
+import requests
+import config
+
+def  wellformedness_verify_request(invoice):
+    return requests.post(config.url + '/invoice/verify/wellformedness', json={
+
+
+
+    })
