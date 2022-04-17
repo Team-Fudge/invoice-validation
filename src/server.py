e1fecab86674fb3e489499b4f160b0cbb78895bb
Trissha <trisshavarman@gmail.com>
2022-04-17 17:04:32 +1000

    correct output for create invoice route

--- src/server.py
+++ src/server.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 import requests
 from src import config
 from src.peppol_validation import check_reference_number, check_date_syntax, check_currency_Code, check_if_buyer_seller_address_exists, check_xml_empty, check_if_one_tax_total_is_provided, check_if_base_quantity_is_positive_number, check_valid
+import xmltodict
 
 # Errors
 from src.error import InputError
@@ -188,7 +189,7 @@ def create_invoice():
     json_data = request.get_json()
     url = 'https://seng-donut-deployment.herokuapp.com/json/convert'
     resp = requests.post(url, json=json_data)
-    return dumps(resp.text)
+    return (resp.text)
     
 
 if __name__ == "__main__":
