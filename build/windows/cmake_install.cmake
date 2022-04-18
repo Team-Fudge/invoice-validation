434087486aa1abbba50ad96e202a3d4adc6e00f0
YudenS <100655745+YudenS@users.noreply.github.com>
2022-04-18 21:07:01 +1000

    added validation routes

--- build/windows/cmake_install.cmake
+++ build/windows/cmake_install.cmake
@@ -42,6 +42,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/plugins/url_launcher_windows/cmake_install.cmake")
+endif()
+
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
     list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
@@ -141,6 +146,40 @@ file(INSTALL DESTINATION "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_fro
         message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
     endif()
 file(INSTALL DESTINATION "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Release" TYPE FILE FILES "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/windows/flutter/ephemeral/flutter_windows.dll")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+     "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Debug/url_launcher_windows_plugin.dll")
+    if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+    if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+file(INSTALL DESTINATION "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Debug" TYPE FILE FILES "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/plugins/url_launcher_windows/Debug/url_launcher_windows_plugin.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+     "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Profile/url_launcher_windows_plugin.dll")
+    if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+    if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+file(INSTALL DESTINATION "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Profile" TYPE FILE FILES "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/plugins/url_launcher_windows/Profile/url_launcher_windows_plugin.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+     "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Release/url_launcher_windows_plugin.dll")
+    if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+    if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+        message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+    endif()
+file(INSTALL DESTINATION "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/runner/Release" TYPE FILE FILES "C:/Users/ysaeu/OneDrive/Desktop/invoice_validation_frontend/build/windows/plugins/url_launcher_windows/Release/url_launcher_windows_plugin.dll")
   endif()
 endif()
 
